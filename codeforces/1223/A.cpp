8183dca9ea5521e6034e32d6fd9d8beaad345a88
Anupam-Roy16 <anu.ruet@gmail.com>
2020-02-12 19:11:00 +0600

    Add solution for problem `A - CME` Link: https://codeforces.com/contest/1223/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1223/submission/70844941

---
+++ codeforces/1223/A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n,a;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a;
+        if(a==2)
+        {
+            cout<<"2"<<endl;
+        }
+        else if((a%2)==0){
+                cout<<"0"<<endl;
+
+        }
+        else{
+            cout<<"1"<<endl;
+        }
+    }
+    return 0;
+}
