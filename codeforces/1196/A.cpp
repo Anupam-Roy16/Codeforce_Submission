7084f174b8d87e2a786c1538f9985f04cbb7313b
Anupam-Roy16 <anu.ruet@gmail.com>
2020-02-13 14:07:00 +0600

    Add solution for problem `A - Three Piles of Candies` Link: https://codeforces.com/contest/1196/problem/A Tags: brute force, constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1196/submission/70940243

---
+++ codeforces/1196/A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    unsigned long long  l,a,b,c,n,i;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a>>b>>c;
+         l=a+b+c;
+         cout<<l/2<<endl;
+
+
+    }
+
+    return 0;
+}
