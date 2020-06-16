c7e9a274073e34dbcb4cd4ed5d944e0dd2b99259
Anupam-Roy16 <anu.ruet@gmail.com>
2020-06-16 11:32:00 +0600

    Add solution for problem `A - A+B (Trial Problem)` Link: https://codeforces.com/contest/1351/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1351/submission/83892683

---
+++ codeforces/1351/A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m,i,j,a,b;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
