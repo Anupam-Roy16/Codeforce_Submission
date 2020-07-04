727f7487007cfeafbc5c833b41176b09ef5b791b
Anupam-Roy16 <anu.ruet@gmail.com>
2020-07-04 14:26:00 +0600

    Add solution for problem `A - Complicated GCD` Link: https://codeforces.com/contest/664/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/664/submission/85906417

---
+++ codeforces/664/A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char a[1001],b[1001];
+    cin>>a>>b;
+    if(strcmp(a,b)==0)
+    {
+        cout<<a<<endl;
+    }
+    else{
+        cout<<1<<endl;
+    }
+    return 0;
+}
