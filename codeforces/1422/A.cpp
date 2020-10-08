795dad3210dfaea1a8f0ffb3c6f8b7cc118b1e16
Anupam-Roy16 <anu.ruet@gmail.com>
2020-10-08 14:31:00 +0600

    Add solution for problem `A - Fence` Link: https://codeforces.com/contest/1422/problem/A Tags: geometry, math, *800 Ref: https://codeforces.com/contest/1422/submission/94895718

---
+++ codeforces/1422/A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    unsigned long long  q,r,j,p;
+    cin>>p;
+    int i;
+    for(i=0;i<p;i++)
+    {
+        cin>>q>>r>>j;
+        cout<<((q+r+j)-1)<<endl;
+    }
+
+    return 0;
+}
