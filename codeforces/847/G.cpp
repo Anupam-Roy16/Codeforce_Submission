bbc76890e74f76b427e3d17a8eceaf272393de9e
Anupam-Roy16 <anu.ruet@gmail.com>
2021-02-24 19:31:00 +0600

    Add solution for problem `G - University Classes` Link: https://codeforces.com/contest/847/problem/G Tags: implementation, *900 Ref: https://codeforces.com/contest/847/submission/108405901

---
+++ codeforces/847/G.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,p,t=-1,i,j;
+    cin>>n;
+    char a[n][7+1];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0;i<7;i++)
+    {
+        p=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[j][i]=='1')
+            {
+                p++;
+            }
+        }
+        if(p>t)
+        {
+            t=p;
+        }
+    }
+    cout<<t<<endl;
+
+
+
+}
+
+
+
+
