3b511b247f3b7fe89311e3d80e219568f91c231d
Anupam-Roy16 <anu.ruet@gmail.com>
2021-07-19 23:31:00 +0600

    Add solution for problem `A - Strange Functions` Link: https://codeforces.com/contest/1455/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1455/submission/123044018

---
+++ codeforces/1455/A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int r;
+    cin>>r;
+    while(r--)
+    {
+        string s;
+        cin>>s;
+        cout<<s.size()<<endl;
+    }
+}
