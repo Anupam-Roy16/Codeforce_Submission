417f5444453d3edd996cdd7d2f929c0d4e1c9622
Anupam-Roy16 <anu.ruet@gmail.com>
2022-04-15 14:14:00 +0600

    Add solution for problem `B - Who's Opposite?` Link: https://codeforces.com/contest/1560/problem/B Tags: math, *800 Ref: https://codeforces.com/contest/1560/submission/153702385

---
+++ codeforces/1560/B.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define d double
+#define si(n) scanf("%d", &n)
+#define sll(n) scanf("%lld", &n)
+#define sull(n) scanf("%llu", &n)
+#define sd(n) scanf("%lf", &n)
+#define ss(s) cin>>s
+#define pf(n) cout<<n<<endl
+#define v(v) vector<int>v
+#define map(p) map<int,int>p
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define forf(i,stop) for(ll i = 0; i < stop; ++i)
+#define forb(i,start) for(ll i = start-1; i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define el '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#include <time.h>
+
+// The main program calls fun() and measures time taken by fun()
+int main()
+{
+	// Calculate the time taken by fun()
+	int r,a,b,n,i,p,q;
+	si(n);
+	while(n--)
+    {
+        si(p),si(q),si(r);
+        a=abs(p-q);
+        a*=2;
+        if((a<p)||(a<q)||(a<r))
+        {
+            pf(-1);
+        }
+        else{
+            r+=(a/2);
+            if(r!=a)pf(r%a);
+            else pf(r);
+        }
+    }
+	return 0;
+}
