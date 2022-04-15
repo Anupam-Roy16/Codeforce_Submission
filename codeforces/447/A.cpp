c9a29a9c4403fd08ba1d5043f88cb1f202a78bba
Anupam-Roy16 <anu.ruet@gmail.com>
2022-04-15 18:31:00 +0600

    Add solution for problem `A - DZY Loves Hash` Link: https://codeforces.com/contest/447/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/447/submission/153721108

---
+++ codeforces/447/A.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define d double
+#define si(n) scanf("%d", &n)
+#define sll(n) scanf("%lld", &n)
+#define sull(n) scanf("%llu", &n)
+#define sd(n) scanf("%lf", &n)
+#define ss(s) cin>>s
+#define pf(n) cout<<n<<endl
+#define v(v) vector<int>v
+#define map(p) map<int,int>p
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define forf(i,stop) for(ll i = 0; i < stop; ++i)
+#define forb(i,start) for(ll i = start-1; i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define el '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#include <time.h>
+
+// The main program calls fun() and measures time taken by fun()
+int main()
+{
+	int j,r,a,b,n,i,p,q,y=-1;
+	si(p),si(q);
+	v(v);
+	for(j=0;j<p;j++)
+    {
+        v.pb(-1);
+    }
+	for(i=0;i<q;i++)
+    {
+        si(r);
+        r%=p;
+        if((v[r]!=-1)&&(y==-1))
+        {
+        y=i+1;
+        }
+        else v[r]=131341;
+
+    }
+    if(y==-1)pf(-1);
+    else pf(y);
+	return 0;
+}
