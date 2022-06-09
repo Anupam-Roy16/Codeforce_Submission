a0a9e285a6fdfa5f5d2c73c44dbe260e7ca4bd4f
Anupam-Roy16 <anu.ruet@gmail.com>
2022-06-09 20:18:00 +0600

    Add solution for problem `A - Odd Set` Link: https://codeforces.com/contest/1542/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1542/submission/160025973

---
+++ codeforces/1542/A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dbl double
+#define si(n) scanf("%d", &n)
+#define sll(n) scanf("%lld", &n)
+#define sull(n) scanf("%llu", &n)
+#define sd(n) scanf("%lf", &n)
+#define ss(s) cin>>s
+#define pf(n) cout<<n<<endl
+#define v(v) vector<int>v
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define forf(i,stop) for(ll i = 0; i < stop; ++i)
+#define forb(i,start) for(ll i = start-1; i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define el '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main()
+{
+   int  n,i,p,q,x,y,l,m,r;
+   si(n);
+   while(n--)
+   {
+       si(m);
+       q=0;
+       for(i=0;i<2*m;i++)
+       {
+           si(p);
+           if(p%2==0)q++;
+       }
+       if(q==m)cout<<"Yes"<<el;
+       else cout<<"No"<<el;
+   }
+   return 0;
+}
+
+
