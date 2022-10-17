7fdb00500bafd531b9ce9e9ca49acd93ecb8ef72
Anupam-Roy16 <anu.ruet@gmail.com>
2022-10-17 21:25:00 +0600

    Add solution for problem `B - Permutation Value` Link: https://codeforces.com/contest/1743/problem/B Tags: constructive algorithms, greedy, *800 Ref: https://codeforces.com/contest/1743/submission/176742251

---
+++ codeforces/1743/B.py
@@ -0,0 +1,24 @@
+n=int(input())
+for i in range(n):
+    p=int(input())
+    print("1",end=" ")
+    for j in range(3,p+1):
+        print(j,end=" ")
+    print("2")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
