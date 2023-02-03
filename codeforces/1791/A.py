fd2f6287792aeb9dd85ca1971ae39e600f7eadf5
Anupam-Roy16 <anu.ruet@gmail.com>
2023-02-03 20:47:00 +0600

    Add solution for problem `A - Codeforces Checking` Link: https://codeforces.com/contest/1791/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1791/submission/191885691

---
+++ codeforces/1791/A.py
@@ -0,0 +1,7 @@
+n=int(input())
+for i in range(n):
+    s=input()
+    if s=='c' or s=='o' or s=='d' or s=='f' or s=='o' or s=='r' or s=='c' or s=='e' or s=='s':
+        print("YES")
+    else:
+        print("NO")
